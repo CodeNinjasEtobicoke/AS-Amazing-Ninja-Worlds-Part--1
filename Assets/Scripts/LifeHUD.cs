a9d97508035afbcefdc0831c0ca516364b2b49d1
CodeNinjasEtobicoke <122121405+Stegosaurus1117@users.noreply.github.com>
2023-08-03 18:20:34 -0400

    Amazing Ninja Worlds Part 1
    Done

---
+++ Assets/Scripts/LifeHUD.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LifeHUD : MonoBehaviour
+{
+    private GameObject[] hearts;
+    private int lives = 3;
+    public GameObject background;
+    // Start is called before the first frame update
+    void Start()
+    {
+        hearts = GameObject.FindGameObjectsWithTag("Heart");
+    }
+
+    public void HurtPlayer()
+    {
+        Debug.Log("OWIE ZOWIE;");
+        lives -= 1;
+        hearts[lives].SetActive(false);
+        
+        if (lives == 0)
+        {
+            background.GetComponent<GameManager>().GameOver();
+        }
+    }
+}
